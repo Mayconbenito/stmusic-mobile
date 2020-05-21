560fe4686e75da06eb259bace7f7a9d1962463d8
Maycon Benito <mayki17321@gmail.com>
2020-05-21 11:49:02 -0300

    feat: enable horizontaly swipe on library screen

--- src/screens/Library/styles.js
+++ src/screens/Library/styles.js
@@ -1,27 +1,24 @@
+import Animated from 'react-native-reanimated';
 import { heightPercentageToDP as hp } from 'react-native-responsive-screen';
 import styled from 'styled-components/native';
 
-export const Container = styled.View`
-  flex: 1;
-  background-color: #141414;
-  padding-horizontal: 10;
-  padding-vertical: 5;
-`;
-
-export const MenuContainer = styled.View`
+export const TabBar = styled.View`
   flex-direction: row;
+  align-items: center;
+  height: 40;
+  padding-top: 5;
+  background-color: #141414;
   justify-content: flex-start;
+  padding-horizontal: 10;
 `;
 
-export const MenuItem = styled.TouchableOpacity`
+export const TabItem = styled.TouchableOpacity`
+  align-items: center;
   margin-right: 10;
-  border-bottom-color: #d99207;
-  padding-bottom: 3;
-  border-bottom-width: ${props => (props.active ? 2 : 0)};
+  border-bottom-color: #fff;
 `;
 
-export const MenuText = styled.Text`
-  color: #fff;
-  font-size: ${hp(2.3)};
+export const TabItemText = styled(Animated.Text)`
   font-weight: bold;
+  font-size: ${hp(2.3)};
 `;
