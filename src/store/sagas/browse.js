b471d69d9c146319b28c6664568d5e62211c6caf
Maycon Benito <mayki17321@gmail.com>
2020-06-08 14:57:01 -0300

    chore: fetch browse lists in parallel

--- src/store/sagas/browse.js
+++ src/store/sagas/browse.js
@@ -1,4 +1,4 @@
-import { put, call, all, takeLatest } from 'redux-saga/effects';
+import { put, all, takeLatest } from 'redux-saga/effects';
 
 import api from '~/services/api';
 
@@ -11,44 +11,44 @@ const { failure, setList } = BrowseActions;
 
 function* fetchBrowse() {
   try {
-    const recentlyPlayed = yield call(api.get, '/me/recently-played', {
-      params: {
-        page: 1,
-        limit: 60,
-      },
-    });
+    const [
+      recentlyPlayed,
+      trending,
+      genres,
+      mostPlayed,
+      mostFollowed,
+    ] = yield Promise.all([
+      api.get('/me/recently-played', {
+        params: {
+          page: 1,
+          limit: 60,
+        },
+      }),
+      api.get('/browse/tracks/trending', {
+        params: {
+          page: 1,
+          limit: 60,
+        },
+      }),
+      api.get('/genres'),
+      api.get('/browse/tracks/most-played', {
+        params: {
+          page: 1,
+          limit: 60,
+        },
+      }),
+      api.get('/browse/artists/most-followed', {
+        params: {
+          page: 1,
+          limit: 60,
+        },
+      }),
+    ]);
 
-    put(setList(recentlyPlayed.data.tracks, 'recentlyPlayed'));
-
-    const trending = yield call(api.get, '/browse/tracks/trending', {
-      params: {
-        page: 1,
-        limit: 60,
-      },
-    });
-
+    yield put(setList(recentlyPlayed.data.tracks, 'recentlyPlayed'));
     yield put(setList(trending.data.tracks, 'trending'));
-
-    const genres = yield call(api.get, '/genres');
-
     yield put(setList(genres.data.genres, 'genres'));
-
-    const mostPlayed = yield call(api.get, '/browse/tracks/most-played', {
-      params: {
-        page: 1,
-        limit: 60,
-      },
-    });
-
     yield put(setList(mostPlayed.data.tracks, 'mostPlayed'));
-
-    const mostFollowed = yield call(api.get, '/browse/artists/most-followed', {
-      params: {
-        page: 1,
-        limit: 60,
-      },
-    });
-
     yield put(setList(mostFollowed.data.artists, 'mostFollowed'));
   } catch (err) {
     yield put(failure(err));
