7e446bfa350e027d45ec03f56eda235c8c85cc37
Maycon Benito <mayki17321@gmail.com>
2021-02-28 23:39:09 -0300

    chore: show splash screen while loading auth state

--- android/app/src/main/java/com/mayconbenito/stmusic/MainActivity.java
+++ android/app/src/main/java/com/mayconbenito/stmusic/MainActivity.java
@@ -1,6 +1,10 @@
 package com.mayconbenito.stmusic;
 
+import android.os.Bundle;
+
 import com.facebook.react.ReactActivity;
+
+import com.zoontek.rnbootsplash.RNBootSplash;
 
 public class MainActivity extends ReactActivity {
 
@@ -11,5 +15,11 @@ public class MainActivity extends ReactActivity {
   @Override
   protected String getMainComponentName() {
     return "stmusic";
+  }
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    RNBootSplash.init(R.drawable.bootsplash, MainActivity.this); // <- display the generated bootsplash.xml drawable over our MainActivity
   }
 }
