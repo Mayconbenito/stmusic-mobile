48cbcea0a20f7e7a0f34ec2e9b02ec5adca0b101
Maycon Benito <mayconbenito21@gmail.com>
2021-04-30 15:11:53 -0300

    fix: a lot of bugs again

--- src/services/TrackPlayerService.js
+++ src/services/TrackPlayerService.js
@@ -3,7 +3,7 @@ import TrackPlayer from 'react-native-track-player';
 import { Creators as PlayerActions } from '~/store/ducks/player';
 
 import store from '../store';
-import api from './api';
+// import api from './api';
 
 export default async () => {
   TrackPlayer.addEventListener('remote-play', () => {
@@ -26,21 +26,21 @@ export default async () => {
     store.dispatch(PlayerActions.reset());
   });
 
-  async function handlePlayerProgress() {
-    const currentTrackId = await TrackPlayer.getCurrentTrack();
+  // async function handlePlayerProgress() {
+  //   const currentTrackId = await TrackPlayer.getCurrentTrack();
 
-    if (currentTrackId) {
-      const count = [];
-      const position = await TrackPlayer.getPosition();
-      const duration = await TrackPlayer.getDuration();
+  //   if (currentTrackId) {
+  //     const count = [];
+  //     const position = await TrackPlayer.getPosition();
+  //     const duration = await TrackPlayer.getDuration();
 
-      const percentage = Math.round((position * 100) / duration || 0);
-      if (!count.find(item => item === currentTrackId) && percentage === 5) {
-        count.push(currentTrackId);
-        await api.post(`/app/tracks/plays/${currentTrackId}`);
-      }
-    }
-  }
+  //     const percentage = Math.round((position * 100) / duration || 0);
+  //     if (!count.find(item => item === currentTrackId) && percentage === 5) {
+  //       count.push(currentTrackId);
+  //       await api.post(`/app/tracks/plays/${currentTrackId}`);
+  //     }
+  //   }
+  // }
 
-  setInterval(() => handlePlayerProgress(), 1000);
+  // setInterval(() => handlePlayerProgress(), 1000);
 };
